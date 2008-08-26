5981a7a2fcc127d5de665f66d0a2927620984f75
Pierre Gueth <pierre@iu-hl-237.(none)>
2008-08-25 17:40:44 -0700

    iiiii

--- human_bot.cpp
+++ human_bot.cpp
@@ -13,8 +13,16 @@ int main(int argc, char *argv[]) {
     if (argc>1) game=parse_game(argv[1]);
     Board *board=choose_game(game);
 
-	Player *player_a=new PlayerBot(PLAYER_1,7,6e5);
-	Player *player_b=new PlayerHuman(PLAYER_2);
+	Player *player_a;
+	Player *player_b;
+    if (argc>1) {
+        player_a=new PlayerBot(PLAYER_1,7,6e5);
+        player_b=new PlayerHuman(PLAYER_2);
+    } else {
+        player_a=new PlayerHuman(PLAYER_1);
+        player_b=new PlayerBot(PLAYER_2,7,6e5);
+    }
+
 
 	play_game(player_a,player_b,board);
 
